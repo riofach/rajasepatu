e82327a640a36a066e06d577489272c4ff4c2087
riofach <fachrio.raditya@gmail.com>
2025-03-19 18:09:47 +0700

    change domain

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@raja-sepatu.laravel.cloud') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@rajasepatu.com') && $this->hasVerifiedEmail();
     }
 
     /**
